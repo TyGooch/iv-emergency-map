55dbb23b19d761d49ffdc23c3d5fc285f4674867
Ty Gooch <gooch.ty@gmail.com>
2017-12-13 08:40:27 -0800

    fixed routes

--- server/index.js
+++ server/index.js
@@ -85,17 +85,19 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-  // remove cacheing
-  res.setHeader('Cache-Control', 'no-cache');
-  next();
-});
+//To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
+// app.use(function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+// 
+//   // remove cacheing
+//   res.setHeader('Cache-Control', 'no-cache');
+//   next();
+// });
 
 // app.get('*', function(request, response) {
 //   response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
@@ -145,6 +147,10 @@ router.route('/emergencies')
 
 //Use router config when making calls to /api
 app.use('/api', router);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(port, host, function() {
   getTweets();
