463256ba2f7e77688998146e397b003a37ec496b
Ty Gooch <gooch.ty@gmail.com>
2017-12-26 04:47:22 -0500

    included bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
