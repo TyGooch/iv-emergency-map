948742cf7e7cd3811f465cd09342fa53d97e1344
Ty Gooch <gooch.ty@gmail.com>
2017-12-26 03:02:09 -0500

    added filtering by emergency type

--- client/src/Components/EmergencyList/EmergencyListItem.js
+++ client/src/Components/EmergencyList/EmergencyListItem.js
@@ -14,11 +14,16 @@ class EmergencyListItem extends Component {
   }
   
   render(){
+    var time = new Date(this.props.time).toString();
+    time = time.slice(0, -15);
+    
     return(
       <div className="EmergencyListItem" style={ style.EmergencyListItem } onClick={ () => this.clickMarker(this.props.marker) }>
         <h3>{ this.props.description }</h3>
         <p>
-          { this.props.time }
+          { time.slice(0,-14) }
+          <br />
+          { time.slice(-9) }
           <br />
           { this.props.address }
         </p>
--- client/src/components/Filter/FilterForm.js
+++ client/src/components/Filter/FilterForm.js
@@ -4,18 +4,45 @@ const handleChange = (filter, updateFilter) => e => (
   updateFilter(filter, parseInt(e.currentTarget.value))
 );
 
-const FilterForm = ({ limit, types, timeBounds, updateFilter}) => { 
-  return(
-  <div>
-    <span className="filter">Filter results:</span>
-    <br/>
-    <label>Maximum Emergencies</label>
-    <input
-      type="number"
-      value={limit}
-      onChange={handleChange('limit', updateFilter)}
-    />
-  </div>
-);}
+const handleTypeToggle = (filter, updateFilter) => e => {
+  // debugger;
+  var types = filter.types;
+  types[filter.type] = !types[filter.type];
+  
+  return updateFilter(filter.types, types)
+}
+
+class FilterForm extends React.Component {
+  
+  render() {
+    var types = this.props.types;
+    var updateFilter = this.props.updateFilter;
+    
+    const typeFilters = Object.keys(types).map(type => (
+      <div>
+      <input type="checkbox"
+      className="toggle"
+      defaultChecked={types[type]}
+      onChange={ handleTypeToggle({types, type}, updateFilter)}/>
+      <label ref="text">{type}</label>
+      </div>
+    ))
+    
+    return(
+      <div>
+        <span className="filter">Filter results:</span>
+        <br/>
+        { typeFilters }
+        <br/>
+        <label>Maximum Emergencies</label>
+        <input
+          type="number"
+          value={this.props.limit}
+          onChange={handleChange('limit', this.props.updateFilter)}
+        />
+      </div>
+    )
+  }
+}
 
 export default FilterForm;
